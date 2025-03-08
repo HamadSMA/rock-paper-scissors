f09bbd9722700bcbd781bcf1947c213b3fe0ca7c
Hamad Alaslani <hamadsmma@gmail.com>
2025-03-09 01:16:27 +0300

    Add psuedocode and code for game round logic

--- README.md
+++ README.md
@@ -11,3 +11,9 @@
 
 - prompt user for choice
 - call a function to evaluate that choice and return rock, paper, or scissors
+
+### pseudocode for game round logic
+
+- a function that evaluates both human choice and computer choice
+- if human choice is rock and computer choice scissors, log a message and increase score
+- repeat previous steps for paper and scissors and then again for computer choice.
--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 "use strict";
 
+let humanScore = 0;
+let computerScore = 0;
+
 function choiceFunc(number) {
-  if (number == 1) {
+  if (number === 1) {
     return "Rock";
-  } else if (number == 2) {
+  } else if (number === 2) {
     return "Paper";
   } else {
     return "Scissors";
@@ -23,4 +26,28 @@ function getHumanChoice() {
         Scissors: 3`)
   );
   return choiceFunc(choice);
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "Rock" && computerChoice === "Scissors") {
+    console.log("You win! Rock beats scissors");
+    humanScore++;
+  } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+    console.log("You win! Paper beats Rock");
+    humanScore++;
+  } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+    console.log("You win! Scissors beat paper");
+    humanScore++;
+  } else if (computerChoice === "Rock" && humanChoice === "Scissors") {
+    console.log("You lose ): Scissors lose to rock");
+    computerScore++;
+  } else if (computerChoice === "Paper" && humanChoice === "Rock") {
+    console.log("You lose ): Rock loses to paper");
+    computerScore++;
+  } else if (computerChoice === "Scissors" && humanChoice === "Paper") {
+    console.log("You lose ): Paper loses scissors");
+    computerScore++;
+  } else {
+    console.log("It's a tie.");
+  }
 }
