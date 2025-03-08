d5abe9e3de0c7a81ebf9bd1db8bd359ab0d5c14d
Hamad Alaslani <hamadsmma@gmail.com>
2025-03-09 00:45:52 +0300

    Add logic for human choice

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 - if number = 1, return rock
 - if number = 2, return paper
 - if number = 3, return scissors
+
+### pseudocode for human logic
+
+- prompt user for choice
+- call a function to evaluate that choice and return rock, paper, or scissors
--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 "use strict";
 
-function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
-  if (randomNumber == 1) {
+function choiceFunc(number) {
+  if (number == 1) {
     return "Rock";
-  } else if (randomNumber == 2) {
+  } else if (number == 2) {
     return "Paper";
   } else {
     return "Scissors";
   }
+}
+
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  return choiceFunc(randomNumber);
+}
+
+function getHumanChoice() {
+  let choice = Number(
+    prompt(`Type a number
+        Rock: 1
+        Paper: 2
+        Scissors: 3`)
+  );
+  return choiceFunc(choice);
 }
