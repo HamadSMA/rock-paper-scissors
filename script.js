1af930320403b15887f38a3fc5b126508c8c55cb
Hamad Alaslani <hamadsmma@gmail.com>
2025-04-04 08:37:25 +0300

    Add logic and UI for rps game

--- script.js
+++ script.js
@@ -1,68 +1,120 @@
 "use strict";
 
+let humanPoint = 0;
 let humanScore = 0;
+let computerPoint = 0;
 let computerScore = 0;
 
-function choiceFunc(number) {
-  if (number === 1) {
+const buttons = document.querySelectorAll("button");
+const msg = document.querySelector(".msg");
+const humanPointUI = document.querySelector(".player-point");
+const computerPointUI = document.querySelector(".computer-point");
+const playerScoreUI = document.querySelector(".player-score");
+const computerScoreUI = document.querySelector(".computer-score");
+const playerRollUI = document.querySelector(".player-roll");
+const computerRollUI = document.querySelector(".computer-roll");
+
+humanPointUI.textContent = humanPoint;
+computerPointUI.textContent = computerPoint;
+playerScoreUI.textContent = humanScore;
+computerScoreUI.textContent = computerScore;
+
+function pointUp(point) {
+  if (point === 1 && humanPoint !== 5) {
+    humanPoint++;
+    humanPointUI.textContent = humanPoint;
+  } else if (point === 0 && computerPoint !== 5) {
+    computerPoint++;
+    computerPointUI.textContent = computerPoint;
+  }
+}
+
+// generate computer choice
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  if (randomNumber === 1) {
     return "Rock";
-  } else if (number === 2) {
+  } else if (randomNumber === 2) {
     return "Paper";
   } else {
     return "Scissors";
   }
 }
 
-function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
-  return choiceFunc(randomNumber);
+// check who has more points after the a round is finished
+function checkWinner(humanPoint, computerPoint) {
+  if (humanPoint > computerPoint) {
+    msg.textContent = "Congratulation! You win! =>";
+    humanScore++;
+    playerScoreUI.textContent = humanScore;
+  } else if (humanPoint < computerPoint) {
+    msg.textContent = "The computer won =<";
+    computerScore++;
+    computerScoreUI.textContent = computerScore;
+  } else {
+    msg.textContent = "It's a tie...";
+  }
 }
 
-function getHumanChoice() {
-  let choice = Number(
-    prompt(`Type a number
-        Rock: 1
-        Paper: 2
-        Scissors: 3`)
-  );
-  return choiceFunc(choice);
-}
-
-function playRound(humanChoice, computerChoice) {
+// check who gets a point after each play
+function checkPoint(humanChoice, computerChoice) {
+  let point = 0;
   if (humanChoice === "Rock" && computerChoice === "Scissors") {
-    console.log("You win! Rock beats scissors");
-    humanScore++;
+    point = 1;
+    pointUp(point);
+    playerRollUI.textContent = humanChoice;
+    computerRollUI.textContent = computerChoice;
   } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-    console.log("You win! Paper beats Rock");
-    humanScore++;
+    point = 1;
+    pointUp(point);
+    playerRollUI.textContent = humanChoice;
+    computerRollUI.textContent = computerChoice;
   } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-    console.log("You win! Scissors beat paper");
-    humanScore++;
+    point = 1;
+    pointUp(point);
+    playerRollUI.textContent = humanChoice;
+    computerRollUI.textContent = computerChoice;
   } else if (computerChoice === "Rock" && humanChoice === "Scissors") {
-    console.log("You lose ): Scissors lose to rock");
-    computerScore++;
+    point = 0;
+    pointUp(point);
+    playerRollUI.textContent = humanChoice;
+    computerRollUI.textContent = computerChoice;
   } else if (computerChoice === "Paper" && humanChoice === "Rock") {
-    console.log("You lose ): Rock loses to paper");
-    computerScore++;
+    point = 0;
+    pointUp(point);
+    playerRollUI.textContent = humanChoice;
+    computerRollUI.textContent = computerChoice;
   } else if (computerChoice === "Scissors" && humanChoice === "Paper") {
-    console.log("You lose ): Paper loses scissors");
-    computerScore++;
+    point = 0;
+    pointUp(point);
+    playerRollUI.textContent = humanChoice;
+    computerRollUI.textContent = computerChoice;
   } else {
-    console.log("It's a tie.");
+    point = 2;
+    playerRollUI.textContent = humanChoice;
+    computerRollUI.textContent = computerChoice;
   }
 }
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice());
-  }
-  if (humanScore > computerScore) {
-    console.log("Congrats, you've won!");
-  } else if (humanScore < computerScore) {
-    console.log("The computer has won ):");
-  } else {
-    console.log("It's a tie ._.");
-  }
-  humanScore = 0;
-  computerScore = 0;
-}
+let playerChoice = "";
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.className === "rock") {
+      playerChoice = "Rock";
+    } else if (e.target.className === "paper") {
+      playerChoice = "Paper";
+    } else if (e.target.className === "scissors") {
+      playerChoice = "Scissors";
+    }
+    if (humanPoint + computerPoint < 5) {
+      checkPoint(playerChoice, getComputerChoice());
+    } else {
+      console.log("total is 5");
+      checkWinner(humanPoint, computerPoint);
+      humanPoint = 0;
+      computerPoint = 0;
+      humanPointUI.textContent = humanPoint;
+      computerPointUI.textContent = computerPoint;
+    }
+  });
+});
